903919ea14b11afa8a2c278d1fddb793d80b14ee
박길호 <pkh0225@ssg.com>
2023-04-09 16:42:36 +0900

    .

--- test/UIDynamicAnimator/DynamicAnimatorViewController.swift
+++ test/UIDynamicAnimator/DynamicAnimatorViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 class DynamicAnimatorViewController: UIViewController , RouterProtocol {
     static var storyboardName: String = "Main"
     
-    @IBOutlet weak var contentView: UIView!
+    @IBOutlet weak var containerView: UIView!
     var blueBoxView: UIView?
     var redBoxView: UIView?
     var purpleBoxView: UIView?
@@ -18,7 +18,7 @@ class DynamicAnimatorViewController: UIViewController , RouterProtocol {
     // 다이나믹스 애니메이터 인스턴스 변수 선언
     var animator: UIDynamicAnimator?
     // 앵커 포인트 현재 위치 저장할 변수
-    var currentLocation: CGPoint?
+    var currentLocation: CGPoint = .zero
     // 항목이 붙어있는 것을 구현할 수 있는 클래스
     var attachment: UIAttachmentBehavior?
 
@@ -28,21 +28,24 @@ class DynamicAnimatorViewController: UIViewController , RouterProtocol {
         var frameRect = CGRect(x: 10, y: 20, width: 80, height: 80)
         blueBoxView = UIView(frame: frameRect)
         blueBoxView?.backgroundColor = UIColor.blue
+        addPanGesture(view: blueBoxView!)
 
         frameRect = CGRect(x: 160, y: 20, width: 60, height: 60)
         redBoxView = UIView(frame: frameRect)
         redBoxView?.backgroundColor = UIColor.red
+        addPanGesture(view: redBoxView!)
 
         frameRect = CGRect(x: 290, y: 20, width: 40, height: 40)
         purpleBoxView = UIView(frame: frameRect)
         purpleBoxView?.backgroundColor = UIColor.purple
+        addPanGesture(view: purpleBoxView!)
 
-        self.contentView.addSubview(blueBoxView!)
-        self.contentView.addSubview(redBoxView!)
-        self.contentView.addSubview(purpleBoxView!)
+        self.containerView.addSubview(blueBoxView!)
+        self.containerView.addSubview(redBoxView!)
+        self.containerView.addSubview(purpleBoxView!)
 
         // 다이나믹스 인스턴스 생성 초기화
-        animator = UIDynamicAnimator(referenceView: self.contentView)
+        animator = UIDynamicAnimator(referenceView: self.containerView)
 
 
         // 두 뷰에 대한 중력 설정
@@ -63,6 +66,7 @@ class DynamicAnimatorViewController: UIViewController , RouterProtocol {
 
         let behavior = UIDynamicItemBehavior(items: [blueBoxView!])
         // 탄성 설정 값이 높을수록 높게 튀어오름
+//        behavior.allowsRotation = false
         behavior.elasticity = 0.5
         animator?.addBehavior(behavior)
 
@@ -83,6 +87,8 @@ class DynamicAnimatorViewController: UIViewController , RouterProtocol {
         boxAttachment2.frequency = 4.0
         // 감쇠 값
         boxAttachment2.damping = 0.0
+
+        boxAttachment2.length = 200
 
         animator?.addBehavior(boxAttachment2)
 
@@ -108,37 +114,45 @@ class DynamicAnimatorViewController: UIViewController , RouterProtocol {
         animator?.addBehavior(push)
 
 
-
-        addPanGesture()
     }
 
-    private func addPanGesture() {
+    private func addPanGesture(view: UIView) {
         let panGesture = UIPanGestureRecognizer(target: self, action: #selector(handlePanGesture(_:)))
-        self.contentView.addGestureRecognizer(panGesture)
+        view.addGestureRecognizer(panGesture)
     }
 
 
     @objc private func handlePanGesture(_ gesture: UIPanGestureRecognizer) {
+        guard let view = gesture.view  else { return }
+        guard let containerView = self.containerView else { return }
         switch gesture.state {
         case .began:
-            currentLocation = gesture.location(in: self.contentView)
-            // 연결 점에 대한 오프셋 설정
-            let offset = UIOffset(horizontal: 20, vertical: 20)
+            currentLocation = gesture.location(in: containerView)
+//            let offset = UIOffset(horizontal: 20, vertical: 20)
             // 오프셋 인자를 제거하면 아이템의 중심에 연결된다.
-            attachment = UIAttachmentBehavior(item: blueBoxView!, offsetFromCenter: offset, attachedToAnchor: currentLocation!)
-
+            attachment = UIAttachmentBehavior(item: view/*, offsetFromCenter: offset*/, attachedToAnchor: currentLocation)
             animator?.addBehavior(attachment!)
         case .changed:
-            currentLocation = gesture.location(in: self.contentView)
-            attachment?.anchorPoint = currentLocation!
-        @unknown default:
-            animator?.removeBehavior(attachment!)
+            currentLocation = gesture.location(in: containerView)
+            attachment?.anchorPoint = currentLocation
+        case .cancelled, .ended:
+            if let attachment = attachment {
+                animator?.removeBehavior(attachment)
+            }
             attachment = nil
+        case .possible:
+            break
+        case .failed:
+            if let attachment = attachment {
+                animator?.removeBehavior(attachment)
+            }
+            attachment = nil
+        @unknown default:
+            break
         }
-
     }
 
-//    // 화면에 터치 시작했을때 앵커 포인터 설정
+    //    // 화면에 터치 시작했을때 앵커 포인터 설정
 //    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
 //        let theTouch = touches.first! as UITouch
 //        currentLocation = theTouch.location(in: self.contentView)
