707451bc2fca42ad4f1f1f319c6f9185123d1378
박길호 <pkh0225@ssg.com>
2023-04-12 08:54:30 +0900

    .

--- test/UIField​Behavior/Field​BehaviorViewController.swift
+++ test/UIField​Behavior/Field​BehaviorViewController.swift
@@ -19,8 +19,6 @@ class Field​BehaviorViewController: UIViewController, RouterProtocol {
     // 충돌 설정
     var collision: UICollisionBehavior?
 
-//    let drag = UIFieldBehavior.dragField()
-
     // 항목이 붙어있는 것을 구현할 수 있는 클래스
     public var attachment: UIAttachmentBehavior?
     // 앵커 포인트 현재 위치 저장할 변수
@@ -35,9 +33,9 @@ class Field​BehaviorViewController: UIViewController, RouterProtocol {
         super.viewDidLoad()
 
         DispatchQueue.main.async {
-            guard let window = UIApplication.shared.windows.filter({ $0.isKeyWindow }).first else { return }
-            let top = window.safeAreaInsets.top
-            let bottom = window.safeAreaInsets.bottom
+//            guard let window = UIApplication.shared.windows.filter({ $0.isKeyWindow }).first else { return }
+//            let top = window.safeAreaInsets.top
+//            let bottom = window.safeAreaInsets.bottom
 
             self.targetView = UIView(frame: CGRect(x: 100, y: 100, width: 70, height: 70))
             self.targetView.backgroundColor = .blue
@@ -46,13 +44,16 @@ class Field​BehaviorViewController: UIViewController, RouterProtocol {
             self.animator = UIDynamicAnimator(referenceView: self.containerView)
             self.animator?.setValue(true, forKey: "debugEnabled")
 
-    //        drag.addItem(self.targetView)
-    //        drag.position = self.targetView.center
-    //        drag.region = UIRegion(size: self.targetView.bounds.size)
-    //        animator?.addBehavior(drag)
+            // 감속지역 설정
+            let drag = UIFieldBehavior.dragField()
+            drag.position = self.targetView.center
+            drag.region = UIRegion(size: self.targetView.bounds.size)
+            self.animator?.addBehavior(drag)
 
             var y: CGFloat = 0
             let size = CGSize(width: self.containerView.frame.width / 1.5, height: self.containerView.frame.height / 4)
+
+            self.addSpringField(center: CGPoint(x: self.containerView.frame.width / 2, y: self.containerView.frame.height / 2), size: size)
 
             for i in 0..<5 {
                 y = (CGFloat(i) * (size.height))
@@ -78,7 +79,7 @@ class Field​BehaviorViewController: UIViewController, RouterProtocol {
 
             }
 
-
+            // 충돌설정
             self.viewBehavior = UIDynamicItemBehavior(items: [self.targetView])
             self.viewBehavior?.allowsRotation = false
             self.viewBehavior?.resistance = 8
@@ -86,7 +87,7 @@ class Field​BehaviorViewController: UIViewController, RouterProtocol {
             self.animator?.addBehavior(self.viewBehavior!)
 
 
-
+            // 경계설정
             self.collision = UICollisionBehavior(items: [self.targetView])
             self.collision?.translatesReferenceBoundsIntoBoundary = true
             self.collision?.setTranslatesReferenceBoundsIntoBoundary(with: UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10))
@@ -98,6 +99,11 @@ class Field​BehaviorViewController: UIViewController, RouterProtocol {
 
     }
 
+
+    /// 자석 지역설정
+    /// - Parameters:
+    ///   - center: 위치
+    ///   - size:   크기
     func addSpringField(center: CGPoint, size: CGSize) {
 //        let scale = CGAffineTransform(scaleX: 0.5, y: 0.5)
 //        let size = self.containerView.bounds.size.applying(scale)
