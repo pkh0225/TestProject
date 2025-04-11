4a50dc19e116390d4fa264a9fc925bcca1077a84
박길호 <pkh0225@ssg.com>
2025-04-11 10:10:26 +0900

    test AnimatedGradientView

--- test/1_View/Gradient/CAGradientLayerTestViewController.swift
+++ test/1_View/Gradient/CAGradientLayerTestViewController.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import SwiftHelper
+import EasyConstraints
 
 class CAGradientLayerTestViewController: UIViewController , RouterProtocol {
 
@@ -23,20 +24,120 @@ class CAGradientLayerTestViewController: UIViewController , RouterProtocol {
     @IBOutlet weak var radialButton: UIButton!
     @IBOutlet weak var conicButton: UIButton!
     
-    var gradientLayer: CAGradientLayer = CAGradientLayer()
+    private var gradientLayer: CAGradientLayer = CAGradientLayer()
+
+    private var animationView: AnimatedGradientView!
+    private var containerView: UIView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         self.title = "Gradient"
         self.hideKeyboardWhenTappedAroundAndCancelsTouchesInView()
 
-        self.gradientLayer.frame = CGRect(x: 0, y: 0, width: self.gradientView.ec.width, height: self.gradientView.ec.height)
-        self.gradientView.layer.addSublayer(gradientLayer)
-
         self.frameWTextField.text = self.gradientView.ec.width.toString
         self.frameHTextField.text = self.gradientView.ec.height.toString
 
+        self.makeAnimationView()
+
         self.setGradient()
+    }
+
+    private func makeAnimationView() {
+        containerView = UIView(frame: CGRect(x: 10, y: 100, width: 100, height: 100))
+        containerView.backgroundColor = .lightGray
+        containerView.addSuperView(self.view)
+            .ec.make()
+            .leading(self.view.leadingAnchor, 10)
+            .top(self.view.topAnchor, 600)
+            .width(100)
+            .height(100)
+
+
+        animationView = AnimatedGradientView(frame: CGRect(x: 10, y: 100, width: 100, height: 100))
+        animationView.cornerRadius(50)
+        animationView.addSuperView(containerView)
+            .ec.make()
+            .leading(containerView.leadingAnchor)
+            .trailing(containerView.trailingAnchor)
+            .top(containerView.topAnchor)
+            .bottom(containerView.bottomAnchor)
+
+
+
+        let btn = UIButton(frame: self.animationView.bounds)
+        self.animationView.addSubViewAutoLayout(btn)
+        btn.addAction(for: .touchUpInside) { btn in
+            let duration = 0.25
+
+            CATransaction.begin()
+            CATransaction.setAnimationDuration(duration)
+            CATransaction.setAnimationTimingFunction(CAMediaTimingFunction(name: .easeInEaseOut))
+
+            UIView.animate(withDuration: duration, delay: 0, options: .curveEaseInOut) {
+                if self.containerView.ec.width == 100 {
+                    self.containerView.ec
+                        .leading(10)
+                        .width(300)
+                        .height(300)
+                }
+                else {
+                    self.containerView.ec
+                        .leading(100)
+                        .width(100)
+                        .height(100)
+                }
+
+//                if self.animationView.ec.width == 100 {
+//                    self.animationView.ec
+//                        .width(300)
+//                        .height(300)
+//
+//                }
+//                else {
+//                    self.animationView.ec
+//                        .width(100)
+//                        .height(100)
+//                }
+                self.view.layoutIfNeeded()
+            }
+
+//            self.animationView.gradientLayer.removeAllAnimations()
+//            // frame 변경을 위한 애니메이션
+//
+//            CATransaction.begin()
+//            CATransaction.setAnimationDuration(0.25)
+//            CATransaction.setAnimationTimingFunction(CAMediaTimingFunction(name: .easeInEaseOut))
+//
+//            let frameAnimation = CAKeyframeAnimation(keyPath: "frame")
+////            frameAnimation.duration = 2.0
+//            frameAnimation.fillMode = .forwards
+//            frameAnimation.isRemovedOnCompletion = false
+//
+//            if self.animationView.gradientLayer.frame.width == 100 {
+//                frameAnimation.values = [NSValue(cgRect: CGRect(x: 0, y: 0, width: 100, height: 100)) ,
+//                NSValue(cgRect: CGRect(x: 0, y: 0, width: 300, height: 300))]
+////                frameAnimation.timingFunction = CAMediaTimingFunction(name: .linear)
+//                self.animationView.gradientLayer.add(frameAnimation, forKey: "frameAnimation")
+//                // 애니메이션 후 실제 frame 값을 업데이트 (중요)
+//                self.animationView.gradientLayer.frame = CGRect(x: 0, y: 0, width: 300, height: 300)
+//            }
+//            else {
+//                frameAnimation.values = [NSValue(cgRect: CGRect(x: 0, y: 0, width: 300, height: 300)),
+//                NSValue(cgRect: CGRect(x: 0, y: 0, width: 100, height: 100))]
+////                frameAnimation.timingFunction = CAMediaTimingFunction(name: .linear)
+//                self.animationView.gradientLayer.add(frameAnimation, forKey: "frameAnimation")
+//                // 애니메이션 후 실제 frame 값을 업데이트 (중요)
+//                self.animationView.gradientLayer.frame = CGRect(x: 0, y: 0, width: 100, height: 100)
+//            }
+//
+//
+//
+            CATransaction.commit()
+
+
+
+
+        }
     }
 
     @IBAction func onApply(_ sender: UIButton) {
@@ -103,7 +204,16 @@ class CAGradientLayerTestViewController: UIViewController , RouterProtocol {
         self.gradientLayer.startPoint = CGPoint(x: self.startPointXTextField.text?.toCGFloat() ?? 0, y: self.startPointYTextField.text?.toCGFloat() ?? 0)
         self.gradientLayer.endPoint = CGPoint(x: self.endPointXTextField.text?.toCGFloat() ?? 0, y: self.endPointYTextField.text?.toCGFloat() ?? 0)
         self.gradientView.layer.addSublayer(gradientLayer)
+
+        self.animationView.gradientLayer.locations = self.gradientLayer.locations
+        self.animationView.gradientLayer.colors = self.gradientLayer.colors
+        self.animationView.gradientLayer.type = self.gradientLayer.type
+        self.animationView.gradientLayer.startPoint = self.gradientLayer.startPoint
+        self.animationView.gradientLayer.endPoint = self.gradientLayer.endPoint
+
         self.view.layoutIfNeeded()
+
+
     }
 }
 
@@ -112,4 +222,39 @@ extension CAGradientLayerTestViewController: UITextFieldDelegate {
         self.dismissKeyboard()
         return true
     }
+}
+
+
+// 커스텀 그라데이션 뷰
+class AnimatedGradientView: UIView {
+
+    let gradientLayer = CAGradientLayer()
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupGradient()
+    }
+
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setupGradient()
+    }
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        gradientLayer.frame = bounds
+        gradientLayer.cornerRadius = bounds.height / 2
+    }
+
+    private func setupGradient() {
+        gradientLayer.colors = [UIColor.purple.cgColor, UIColor.orange.cgColor]
+        gradientLayer.startPoint = CGPoint(x: 0.0, y: 0.0)
+        gradientLayer.endPoint = CGPoint(x: 1.0, y: 1.0)
+        gradientLayer.frame = bounds
+        layer.addSublayer(gradientLayer)
+    }
+
+    func cornerRadius(_ value: CGFloat) {
+        gradientLayer.cornerRadius = value
+    }
+
 }
